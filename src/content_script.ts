b28bdc0a46ca8b23de6a4325b2f6a15ccef41550
Joe <joe@coder.com>
2021-11-25 19:41:46 +0000

    check testing

--- src/content_script.ts
+++ src/content_script.ts
@@ -1,1 +1,13 @@
-alert("Hello from your Chrome extension!")
+/*
+NOTES
+I need to first write a function which checks if the email
+exists in the DOM.
+
+*/
+
+export function init() {
+  console.log("initializing chrome extension...");
+  alert("Hello from your Chrome extension!");
+}
+
+init();
