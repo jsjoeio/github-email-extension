5fa3d0129ca6e817d5207e2b1e09efe0307cb172
Joe <joe@coder.com>
2021-11-26 22:09:01 +0000

    feat: add buildEmailElement

--- src/content_script.ts
+++ src/content_script.ts
@@ -3,7 +3,7 @@ import { Octokit } from "@octokit/rest";
 NOTES
 
 Next steps:
-- insertEmailIntoDOM
+- refactor buildEmailElement
 - add Copy to clipboard into DOM
 
 */
@@ -71,16 +71,16 @@ export function insertEmailIntoDOM(_document: Document, email: string) {
   // implement
   const V_CARD_CLASS = ".vcard-details";
   const vCardElement = _document.querySelector(V_CARD_CLASS);
-  const buildEmailElement = (
-    email: string
-  ) => `<li itemprop="email" aria-label="Email: ${email}" class="vcard-detail pt-1 css-truncate css-truncate-target "><svg class="octicon octicon-mail" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M1.75 2A1.75 1.75 0 000 3.75v.736a.75.75 0 000 .027v7.737C0 13.216.784 14 1.75 14h12.5A1.75 1.75 0 0016 12.25v-8.5A1.75 1.75 0 0014.25 2H1.75zM14.5 4.07v-.32a.25.25 0 00-.25-.25H1.75a.25.25 0 00-.25.25v.32L8 7.88l6.5-3.81zm-13 1.74v6.441c0 .138.112.25.25.25h12.5a.25.25 0 00.25-.25V5.809L8.38 9.397a.75.75 0 01-.76 0L1.5 5.809z"></path></svg>
-  <a class="u-email Link--primary " href="mailto:${email}">${email}</a>
-</li>`;
 
   const emailElement = buildEmailElement(email);
 
   vCardElement?.insertAdjacentHTML("beforeend", emailElement);
 }
+export const buildEmailElement = (
+  email: string
+) => `<li itemprop="email" aria-label="Email: ${email}" class="vcard-detail pt-1 css-truncate css-truncate-target "><svg class="octicon octicon-mail" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M1.75 2A1.75 1.75 0 000 3.75v.736a.75.75 0 000 .027v7.737C0 13.216.784 14 1.75 14h12.5A1.75 1.75 0 0016 12.25v-8.5A1.75 1.75 0 0014.25 2H1.75zM14.5 4.07v-.32a.25.25 0 00-.25-.25H1.75a.25.25 0 00-.25.25v.32L8 7.88l6.5-3.81zm-13 1.74v6.441c0 .138.112.25.25.25h12.5a.25.25 0 00.25-.25V5.809L8.38 9.397a.75.75 0 01-.76 0L1.5 5.809z"></path></svg>
+  <a class="u-email Link--primary " href="mailto:${email}">${email}</a>
+</li>`;
 
 export function init() {
   console.log("initializing chrome extension...");
